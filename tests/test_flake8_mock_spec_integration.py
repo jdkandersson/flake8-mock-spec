93eba8ee686171e010de405b5f9a065268ded87a
David Andersson <david@jdkandersson.com>
2023-01-14 14:29:46 +1100

    add integration tests

---
+++ tests/test_flake8_mock_spec_integration.py
@@ -0,0 +1,107 @@
+"""Integration tests for plugin."""
+
+import subprocess
+import sys
+from pathlib import Path
+
+import pytest
+
+from flake8_mock_spec import MOCK_SPEC_CODE, MAGIC_MOCK_SPEC_CODE, MOCK_SPEC_MSG
+
+
+def test_help():
+    """
+    given: linter
+    when: the flake8 help message is generated
+    then: plugin is registered with flake8
+    """
+    with subprocess.Popen(
+        f"{sys.executable} -m flake8 --help",
+        stdout=subprocess.PIPE,
+        shell=True,
+    ) as proc:
+        stdout = proc.communicate()[0].decode(encoding="utf-8")
+
+        assert "flake8-mock-spec" in stdout
+
+
+def create_code_file(code: str, base_path: Path) -> Path:
+    """Create the code file with the given code.
+
+    Args:
+        code: The code to write to the file.
+        base_path: The path to create the file within
+
+    Returns:
+        The path to the code file.
+    """
+    (code_file := base_path / "test_source.py").write_text(f'"""Docstring."""\n\n{code}')
+    return code_file
+
+
+def test_fail(tmp_path: Path):
+    """
+    given: file with Python code that fails the linting
+    when: flake8 is run against the code
+    then: the process exits with non-zero code and includes the error message
+    """
+    code_file = create_code_file("from unittest import mock\n\nmock.Mock()\n", tmp_path)
+
+    with subprocess.Popen(
+        f"{sys.executable} -m flake8 {code_file}",
+        stdout=subprocess.PIPE,
+        shell=True,
+    ) as proc:
+        stdout = proc.communicate()[0].decode(encoding="utf-8")
+
+        assert MOCK_SPEC_MSG in stdout
+        assert proc.returncode
+
+
+@pytest.mark.parametrize(
+    "code",
+    [
+        pytest.param(
+            """
+from unittest import mock
+
+mock.Mock(spec=1)
+""",
+            id="default",
+        ),
+        pytest.param(
+            f"""
+from unittest import mock
+
+mock.Mock()  # noqa: {MOCK_SPEC_CODE}
+""",
+            id=f"{MOCK_SPEC_CODE} disabled",
+        ),
+        pytest.param(
+            f"""
+from unittest import mock
+
+mock.MagicMock()  # noqa: {MAGIC_MOCK_SPEC_CODE}
+""",
+            id=f"{MAGIC_MOCK_SPEC_CODE} disabled",
+        ),
+    ],
+)
+def test_pass(code: str, tmp_path: Path):
+    """
+    given: file with Python code that passes the linting
+    when: flake8 is run against the code
+    then: the process exits with zero code and empty stdout
+    """
+    code_file = create_code_file(code, tmp_path)
+    (config_file := tmp_path / ".flake8").touch()
+
+    with subprocess.Popen(
+        (f"{sys.executable} -m flake8 {code_file} " f"--config {config_file}"),
+        stdout=subprocess.PIPE,
+        shell=True,
+    ) as proc:
+        stdout = proc.communicate()[0].decode(encoding="utf-8")
+
+        assert not stdout, stdout
+        assert not proc.returncode
